a13d70bcdcfbbe5f117728ce2ffc34a5f223be61
Christina Dong <christinadongla@yahoo.com>
2019-01-11 06:08:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -33,11 +33,11 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
-  kittens.slice(0,2);
+  var kittens = kittens.slice(0, kittens.length -1);
   return kittens;
 }
 
 function removeFirstKitten() {
-  kittens.slice(1);
+  var kittens = kittens.slice(1);
   return kittens;
 }
